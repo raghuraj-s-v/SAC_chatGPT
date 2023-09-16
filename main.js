b6dd244887540b8fcbca564518285acd2f4f7f64
raghuraj-s-v <87940605+raghuraj-s-v@users.noreply.github.com>
2023-09-17 00:00:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,43 @@
 var ajaxCall = (key, url, prompt) => {
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        url: url,
-        type: "POST",
-        dataType: "json",
-        data: JSON.stringify({
-          model: "text-davinci-002",
-          prompt: prompt,
-          max_tokens: 1024,
-          n: 1,
-          temperature: 0.5,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${key}`,
-        },
-        crossDomain: true,
-        success: function (response, status, xhr) {
-          resolve({ response, status, xhr });
-        },
-        error: function (xhr, status, error) {
-          const err = new Error('xhr error');
-          err.status = xhr.status;
-          reject(err);
-        },
-      });
-    });
-  };
-  
-  const url = "https://api.openai.com/v1";
-  
-  (function () {
-    const template = document.createElement("template");
-    template.innerHTML = `
-        <style>
-        </style>
-        <div id="root" style="width: 100%; height: 100%;">
-        </div>
-      `;
-    class MainWebComponent extends HTMLElement {
-      async post(APIKey, endPoint, prompt) {
-        const { response } = await ajaxCall(
-          APIKey,
-          `${url}/${endPoint}`,
-          prompt
-        );
-        //console.log(response.choices[0].text);
-        return response.choices[0].text;
-      }
+fetch(url,{
+  method:"POST",
+  headers:{
+      'Content-Type': "application/json",
+      Authorization: `Bearer ${key}`
+  },
+  body:JSON.stringify({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 1024,
+      n: 1,
+      temperature: 0.5,
+    })
+  }).then((res)=> {
+    return res.json();
+  }).catch((err)=>{
+    console.log(err);
+  })
+};
+
+const url = "https://api.openai.com/v1";
+
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `;
+  class MainWebComponent extends HTMLElement {
+    async post(apiKey, endpoint, prompt) {
+      const response = await ajaxCall(
+        apiKey,
+        `${url}/${endpoint}`,
+        prompt
+      );
+      return response.choices[0].text;
     }
-    customElements.define("custom-widget", MainWebComponent);
-  })();
+  }
+  customElements.define("custom-widget", MainWebComponent);
+})();
