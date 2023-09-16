f75e8f10d7aacc66b58d9526301e51aac29f99e6
raghuraj-s-v <87940605+raghuraj-s-v@users.noreply.github.com>
2023-09-17 00:41:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,23 @@
 var ajaxCall = (key, url, prompt) => {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: url,
-      type: "POST",
-      dataType: "json",
-      data: JSON.stringify({
-        model: "text-davinci-002",
-        prompt: prompt,
-        max_tokens: 1024,
-        n: 1,
-        temperature: 0.5,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${key}`,
-      },
-      crossDomain: true,
-      success: function (response, status, xhr) {
-        resolve({ response, status, xhr });
-      },
-      error: function (xhr, status, error) {
-        const err = new Error('xhr error');
-        err.status = xhr.status;
-        reject(err);
-      },
-    });
-  });
+fetch(url,{
+  method:"POST",
+  headers:{
+      'Content-Type': "application/json",
+      Authorization: `Bearer ${key}`
+  },
+  body:JSON.stringify({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 1024,
+      n: 1,
+      temperature: 0.5,
+    })
+  }).then((res)=> {
+    console.log(res.json());
+    return res.json();
+  }).catch((err)=>{
+    console.log(err);
+  })
 };
 
 const url = "https://api.openai.com/v1";
@@ -40,12 +32,11 @@ const url = "https://api.openai.com/v1";
     `;
   class MainWebComponent extends HTMLElement {
     async post(apiKey, endpoint, prompt) {
-      const { response } = await ajaxCall(
+      const {response} = await ajaxCall(
         apiKey,
         `${url}/${endpoint}`,
         prompt
       );
-      //console.log(response.choices[0].text);
       return response.choices[0].text;
     }
   }
