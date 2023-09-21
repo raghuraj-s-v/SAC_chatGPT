2cc2703a0ae6ac1ea6ba8ebb3edfa14f7764ba5e
raghuraj-s-v <87940605+raghuraj-s-v@users.noreply.github.com>
2023-09-21 11:30:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-var ajaxCall = (key, url, prompt) => {
-fetch(url,{
-  method:"POST",
-  headers:{
-      'Content-Type': "application/json",
-      Authorization: `Bearer ${key}`
-  },
-  body:JSON.stringify({
-      model: "text-davinci-002",
-      prompt: prompt,
-      max_tokens: 1024,
-      n: 1,
-      temperature: 0.5,
-    })
-  }).then((res)=> {
-    console.log(res);
-    return res;
-  }).catch((err)=>{
-    console.log(err);
-  })
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const ajaxCall = async (key, url, prompt) => {
+  let retryCount = 0;
+  const maxRetries = 5;
+  const retryDelay = 1000; // You can adjust the delay time as needed
+  
+  while (retryCount < maxRetries) {
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${key}`,
+        },
+        body: JSON.stringify({
+          model: 'text-davinci-002',
+          prompt: prompt,
+          max_tokens: 1024,
+          n: 1,
+          temperature: 0.5,
+        }),
+      });
+
+      if (response.status === 429) {
+        // If you get a rate limit exceeded error, wait and retry
+        await sleep(retryDelay);
+        retryCount++;
+      } else {
+        return response.json();
+      }
+    } catch (err) {
+      console.error(err);
+      retryCount++;
+      await sleep(retryDelay);
+    }
+  }
+
+  throw new Error('Max retry count reached');
 };
 
 const url = "https://api.openai.com/v1";
@@ -32,7 +50,7 @@ const url = "https://api.openai.com/v1";
     `;
   class MainWebComponent extends HTMLElement {
     async post(apiKey, endpoint, prompt) {
-      const {response} = await ajaxCall(
+      const response = await ajaxCall(
         apiKey,
         `${url}/${endpoint}`,
         prompt
